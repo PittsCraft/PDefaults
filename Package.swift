13762223c455f87adc3f5444d5be3d5cc856ac42
Pierre Mardon <pierre.mardon@gmail.com>
2023-06-28 15:30:50 +0200

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -6,22 +6,19 @@ import PackageDescription
 let package = Package(
     name: "PDefaults",
     platforms: [
-            .macOS(.v12),
-            .iOS(.v13)
-        ],
+        .macOS(.v12),
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PDefaults",
-            targets: ["PDefaults"]),
+            targets: ["PDefaults"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/realm/SwiftLint", exact: "0.52.3")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PDefaults",
             dependencies: [],
